fd53d3e3def7d68dd8342d2f89c030b32a30f620
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-03-21 13:59:03 +0100

    state is now stored in the local storage

--- src/context/GameStateProvider.jsx
+++ src/context/GameStateProvider.jsx
@@ -1,9 +1,23 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import { gameStateContext } from "./gameStateContext";
 import { gameStateReducer, initialGameState } from "./gameStateContext";
 
 const useGameStateProvider = () => {
-  const [gameState, dispatch] = useReducer(gameStateReducer, initialGameState);
+  // When this renders we want to fetch the gamestate stored in local storage.
+  const getInitialState = () => {
+    const storedGameState = localStorage.getItem("storedGameState");
+    // If there is no gamestate we just return initial state
+    return storedGameState ? JSON.parse(storedGameState) : initialGameState;
+  };
+
+  const initialState = getInitialState();
+  // We pass the inital state to the useReducer.
+  const [gameState, dispatch] = useReducer(gameStateReducer, initialState);
+
+  useEffect(() => {
+    // Store the state in localStorage on state updates
+    localStorage.setItem("storedGameState", JSON.stringify(gameState));
+  }, [gameState]);
 
   return { gameState, dispatch };
 };
