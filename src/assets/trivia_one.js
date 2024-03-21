d80f44eddfdd07bb8d7c9b6d7a85d1b3ea681a9a
Zhouhua Hu <zhouhua.hu@gmail.com>
2024-03-21 14:21:59 +0100

    added questions

---
+++ src/assets/trivia_one.js
@@ -0,0 +1,57 @@
+export default {
+  name: "",
+  rounds: [
+    {
+      theme: "algemene kennis",
+      questions: [
+        {
+          question:
+            "Waar staat de afkorting BOB voor als we het hebben over iemand die niet drinkt?",
+          answer: "Bewust Onbeschonken Bestuurder",
+        },
+        {
+          question: "Wie schreef het toneelstuk 'Romeo en Julia'?",
+          answer: "William Shakespeare",
+        },
+        {
+          question: "Van welke soort fruit heb je Fuji, Jazz en Kanzi?",
+          answer: "Appel",
+        },
+        {
+          question: "Wat is de hoofdstad van de provincie Gelderland?",
+          answer: "Arnhem",
+        },
+        {
+          question: "Welke Amerikaan regisseerde de film Pulp Fiction?",
+          answer: "Quentin Tarantino",
+        },
+        {
+          question: "Van welke plant wordt Tequila gemaakt?",
+          answer: "Agave",
+        },
+        {
+          question:
+            "Welke planeet uit ons zonnestelsel staat het dichts bij de zon?",
+          answer: "Mercurius",
+        },
+        {
+          question: "Hoe luidt de stelling van Pythagoras?",
+          answer: "a2 + b2 = c2",
+        },
+        {
+          question:
+            "Welk Chinese stad wordt gezien als oorsprong van het Coronavirus?",
+          answer: "Wuhan",
+        },
+        {
+          question: "Welk land heeft het grootste inwoneraantal in Europa?",
+          answer: "Rusland",
+        },
+        {
+          question: `Wie schreef het boek "Don Quichot"?`,
+          answer: "Miguel de Cervantes",
+        },
+      ],
+    },
+  ],
+};
